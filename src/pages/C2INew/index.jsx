422756c24a270c027e43922886fbca72a58aea8e
zertow <1181527496@qq.com>
2022-05-21 16:48:06 +0800

    prb

--- src/pages/C2INew/index.jsx
+++ src/pages/C2INew/index.jsx
@@ -4,55 +4,55 @@ import { InputNumber, Checkbox, Space } from 'antd';
 
 export default class C2INew extends Component {
     state = {
-        keyboard:true,
-        value:0
+        keyboard: true,
+        value: 0
     }
 
     limitDecimals = (value) => {
         return value.replace(/^(0+)|[^\d]+/g, '');
     }
 
-    confirm = () =>{
+    confirm = () => {
         const value = this.state.value
         axios({
-            method:'post',
-            url:'/data/generate_tbC2Inew',
-            params:{
-                n:value
+            method: 'post',
+            url: '/data/generate_tbC2Inew',
+            params: {
+                n: value
             }
         })
-        .then(
-            (res)=>{
-                console.log(res)
-            }
-        )
-        .catch(
-            (err)=>{
-                console.log(err)
-            }
-        )
+            .then(
+                (res) => {
+                    console.log(res)
+                }
+            )
+            .catch(
+                (err) => {
+                    console.log(err)
+                }
+            )
     }
     render() {
         const keyboard = this.state.keyboard
         return (
             <div>
                 <Space>
-                    <InputNumber 
+                    <InputNumber style={{ width: 150 }}
                         min={0}
                         defaultValue={0}
-                        keyboard={keyboard} 
+                        keyboard={keyboard}
                         formatter={this.limitDecimals}
                         parser={this.limitDecimals}
                         placeholder="请输入测量值对阈值"
-                        onChange={(value)=>{
-                            this.setState({value:value})
+                        onChange={(value) => {
+                            this.setState({ value: value })
                         }}
                     />
                     <Checkbox
                         onChange={() => {
-                            this.setState({keyboard:!keyboard})
+                            this.setState({ keyboard: !keyboard })
                         }}
-                    checked={keyboard}
+                        checked={keyboard}
                     >
                         Toggle keyboard
                     </Checkbox>
