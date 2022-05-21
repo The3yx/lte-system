fa3e66295b5d7233810b8281f17dedc072c2423e
zertow <1181527496@qq.com>
2022-05-21 13:09:36 +0800

    kpi

---
+++ .VSCodeCounter/results.md
@@ -0,0 +1,40 @@
+# Summary
+
+Date : 2022-05-21 12:53:06
+
+Directory d:\lte-system\src
+
+Total : 23 files,  1589 codes, 140 comments, 133 blanks, all 1862 lines
+
+[details](details.md)
+
+## Languages
+| language | files | code | comment | blank | total |
+| :--- | ---: | ---: | ---: | ---: | ---: |
+| JavaScript React | 13 | 1,284 | 79 | 99 | 1,462 |
+| JavaScript | 7 | 129 | 51 | 24 | 204 |
+| Less | 2 | 128 | 10 | 4 | 142 |
+| CSS | 1 | 48 | 0 | 6 | 54 |
+
+## Directories
+| path | files | code | comment | blank | total |
+| :--- | ---: | ---: | ---: | ---: | ---: |
+| . | 23 | 1,589 | 140 | 133 | 1,862 |
+| pages | 15 | 1,412 | 89 | 103 | 1,604 |
+| pages\Admin | 1 | 100 | 9 | 9 | 118 |
+| pages\C2I | 1 | 22 | 0 | 5 | 27 |
+| pages\Community | 1 | 124 | 6 | 8 | 138 |
+| pages\DataImport | 1 | 93 | 7 | 12 | 112 |
+| pages\Enodeb | 1 | 128 | 6 | 9 | 143 |
+| pages\ExportData | 1 | 74 | 6 | 13 | 93 |
+| pages\Kpi | 1 | 183 | 5 | 14 | 202 |
+| pages\Login | 2 | 198 | 19 | 7 | 224 |
+| pages\NotFound | 1 | 8 | 0 | 2 | 10 |
+| pages\Person | 1 | 106 | 4 | 2 | 112 |
+| pages\Prb | 1 | 8 | 1 | 2 | 11 |
+| pages\Register | 2 | 238 | 21 | 9 | 268 |
+| pages\ServerConnection | 1 | 130 | 5 | 11 | 146 |
+| redux | 4 | 76 | 29 | 16 | 121 |
+| utils | 1 | 13 | 18 | 2 | 33 |
+
+[details](details.md)
--- src/pages/Kpi/index.jsx
+++ src/pages/Kpi/index.jsx
@@ -1,10 +1,182 @@
+/*
+@author:zertow
+@time:4.29 19:38
+*/
 import React, { Component } from 'react'
+import { Button } from 'antd';
+import axios from 'axios'
+import { Select } from 'antd';
+import { Descriptions, DatePicker, Space } from 'antd';
+import ReactEcharts from 'echarts-for-react';
+const { Option } = Select;
+
+const { RangePicker } = DatePicker;
+class SearchInput extends React.Component {
+    state = {
+        data: [],
+        value: undefined,
+        isLoading: 0,
+        datestring: [],
+        type: "RCCConnSUCC",
+        x: [],
+        y: [],
+    };
+
+    handleChange = value => {
+        this.setState({ value });
+    };
+    click = () => {
+
+        axios.get(
+            '/data/kpi/detail', {
+            params: {
+                "name": this.state.value,
+                "choice": this.state.type,
+                "start_time": this.state.datestring[0].slice(0, 10),
+                "end_time": this.state.datestring[1].slice(0, 10)
+            }
+        }
+        ).then(body => {
+            this.setState({ tablename: body.data })
+            console.log(this.state.tablename)
+            var new_x = []
+            var new_y = []
+
+            console.log(this.state.type)
+            for (var i in this.state.tablename) {
+                new_x.push(this.state.tablename[i]["StartTime"])
+                if (this.state.type != "RCCConnRATE")
+                    new_y.push(this.state.tablename[i]["Data"] / 10000)
+                else {
+                    new_y.push(this.state.tablename[i]["Data"] * 100)
+                }
+            }
+            this.setState({ x: new_x, y: new_y })
+
+            console.log(this.state.x)
+
+            console.log(this.state.y)
+        });
+        console.log(1)
+        this.setState({ isLoading: true })
+        console.log(this.state)
+    }
+    onChange = (date, dateString) => {
+        this.setState({ datestring: dateString })
+    }
+    gettype = (val) => {
+
+        this.setState({ type: val })
+    }
+    getOption = () => {
+        let option = {
+            tooltip: {
+                trigger: 'axis',
+                axisPointer: {
+                    type: 'cross',
+                    crossStyle: {
+                        color: '#999'
+                    }
+                }
+            },
+            toolbox: {
+                feature: {
+                    dataView: { show: true, readOnly: false },
+                    magicType: { show: true, type: ['line', 'bar'] },
+                    restore: { show: true },
+                    saveAsImage: { show: true }
+                }
+            },
+            legend: {
+                data: ["RCCConnRATE", "RCCConnATT", "RCCConnSUC"]
+            },
+            xAxis: [
+                {
+                    type: 'category',
+                    data: this.state.x,
+                    axisPointer: {
+                        type: 'shadow'
+                    }
+                }
+            ],
+            yAxis: [
+                {
+                    type: 'value',
+                    name: this.state.type != "RCCConnRATE" ? (this.state.type + " 万") : ("RCCConnRATE %"),
+                    min: 0,
+                    max: this.state.type != "RCCConnRATE" ? 10 : 100,
+                    interval: this.state.type != "RCCConnRATE" ? 1 : 10,
+                    axisLabel: {
+                        formatter: '{value}'
+                    }
+                },
+            ],
+            series: [
+                {
+                    name: '蒸发量',
+                    type: 'line',
+                    data: this.state.y
+                },
+            ]
+        };
+        return option
+    }
+    render() {
+
+        const isLoading = this.state.isLoading
+        const options = this.props.table.map(d => <Option value={d["SECTOR_NAME"]}>{d["SECTOR_NAME"]}</Option>);
+        console.log(isLoading)
+        return (
+            <div>
+                <Select
+                    style={{ width: 150 }}
+                    defaultValue="RCCConnSUCC"    //选择器默认显示
+                    onChange={this.gettype}
+                >
+                    <Option key='RCCConnSUCC' value='RCCConnSUCC'>RCCConnSUCC</Option>
+                    <Option key='RCCConnATT' value='RCCConnATT'>RCCConnATT</Option>
+                    <Option key='RCCConnRATE' value='RCCConnRATE'>RCCConnRATE</Option>
+                </Select>
+                <Select
+                    showSearch
+                    optionFilterProp="children"
+                    style={{ width: 200 }}
+                    onChange={this.handleChange}
+                >
+                    {options}
+                </Select><RangePicker showTime onChange={this.onChange} />
+                <Button onClick={this.click}>查询</Button>
+                <ReactEcharts style={{ display: this.state.isLoading == 0 ? 'none' : 'block' }} option={this.getOption()} style={{ width: '500px' }} /></div>
+        );
+    }
+}
 
 export default class Kpi extends Component {
+
+    //TODO:表列表需要从服务端获取，应该写在willMount
+    state = {
+        tableName: "id",
+        tableid: [],
+        tablename: [],
+    }
+    componentDidMount() {
+        axios.get(
+            '/data/sector', {
+            params: {
+                "choice": "name"
+            }
+        }
+        ).then(body => {
+            this.setState({ tablename: body.data })
+            console.log(this.state.tablename)
+        });
+    }
+
     render() {
-        // console.log('About组件收到的props是',this.props);
         return (
-            <h3>我是com的内容</h3>
+            <div>
+                <SearchInput table={this.state.tablename} placeholder="input search text" style={{ width: 200 }} />
+            </div>
         )
     }
 }
