8d69eb1dc1412f5622d1ddc90badf111caeabeb7
The3yx <1340175409@qq.com>
2022-05-29 19:35:00 +0800

    add c2inew and c2i3

--- src/pages/C2I3/index.jsx
+++ src/pages/C2I3/index.jsx
@@ -1,59 +1,104 @@
 import axios from 'axios'
 import React, { Component } from 'react'
 import { InputNumber, Checkbox, Space } from 'antd';
+import { Table, Tag} from 'antd';
+import Papa from "papaparse"
+
 
 //TODO:添加一点后端返回后，界面的样式
-export default class C2I3 extends Component {
+export default class C2INew extends Component {
     state = {
-        keyboard:true,
-        value:0
+        data:[],
+        keyboard: true,
+        value: 0
     }
 
     limitDecimals = (value) => {
         return value.replace(/^(0+)|[^\d]+/g, '');
     }
 
-    confirm = () =>{
+    confirm = () => {
         const value = this.state.value
         axios({
-            method:'post',
-            url:'/data/generate_tbC2I3',
-            params:{
-                x:value
+            method: 'post',
+            url: '/data/generate_tbC2I3',
+            params: {
+                x: value
             }
         })
-        .then(
-            (res)=>{
-                console.log(res)
-            }
-        )
-        .catch(
-            (err)=>{
-                console.log(err)
-            }
-        )
+            .then(
+                (res) => {
+                    console.log(1111,res)
+                    axios({
+                        method:'get',
+                        url:'/data/download',
+                        params:{
+                            table:"tbc2i3",
+                        }
+                    })
+                        .then(
+                            async (res) => {
+                                
+                                fetch(res.data[0])
+                                    .then(
+                                        (response) => {
+                                            console.log(response)
+                                            let reader = response.body.getReader();
+                                            let decoder = new TextDecoder('utf-8');
+                            
+                                            reader.read()
+                                                .then(
+                                                    (result) => {
+                                                        //console.log(decoder.decode(result.value));
+                                                        const csv = decoder.decode(result.value)
+                                                        const parsedCsv = Papa.parse(csv, {header:true})
+                                                        this.setState({data:parsedCsv.data})
+                                                        //console.log(parsedCsv.meta)   ->表头
+
+                                                });
+                                    });
+
+                            }
+                        )
+                        .catch(
+                            (err) => {
+                                console.log(err)
+                            }
+                        )
+                }
+            )
+            .catch(
+                (err) => {
+                    console.log(err)
+                }
+            )
+        
     }
     render() {
         const keyboard = this.state.keyboard
+        var columns = []
+        for (var j in this.state.data[0]) {
+            columns.push({ title: j, dataIndex: j, key: j })
+        }
         return (
             <div>
                 <Space>
-                    <InputNumber 
+                    <InputNumber style={{ width: 150 }}
                         min={0}
                         defaultValue={0}
-                        keyboard={keyboard} 
+                        keyboard={keyboard}
                         formatter={this.limitDecimals}
                         parser={this.limitDecimals}
-                        placeholder="请输入阈值x"
-                        onChange={(value)=>{
-                            this.setState({value:value})
+                        placeholder="请输入阈值参数x"
+                        onChange={(value) => {
+                            this.setState({ value: value })
                         }}
                     />
                     <Checkbox
                         onChange={() => {
-                            this.setState({keyboard:!keyboard})
+                            this.setState({ keyboard: !keyboard })
                         }}
-                    checked={keyboard}
+                        checked={keyboard}
                     >
                         Toggle keyboard
                     </Checkbox>
@@ -61,6 +106,11 @@ export default class C2I3 extends Component {
                 <button onClick={this.confirm}>
                     确认
                 </button>
+                <Table
+                    style={{display: this.state.data === [] ? 'none' : 'block' }}
+                    columns={columns}
+                    dataSource={this.state.data}/>
+
             </div>
 
         )
--- src/pages/C2INew/index.jsx
+++ src/pages/C2INew/index.jsx
@@ -1,11 +1,14 @@
 import axios from 'axios'
 import React, { Component } from 'react'
 import { InputNumber, Checkbox, Space } from 'antd';
+import { Table, Tag} from 'antd';
+import Papa from "papaparse"
 
 
 //TODO:添加一点后端返回后，界面的样式
 export default class C2INew extends Component {
     state = {
+        data:[],
         keyboard: true,
         value: 0
     }
@@ -25,7 +28,43 @@ export default class C2INew extends Component {
         })
             .then(
                 (res) => {
-                    console.log(res)
+                    console.log(1111,res)
+                    axios({
+                        method:'get',
+                        url:'/data/download',
+                        params:{
+                            table:"tbc2inew",
+                        }
+                    })
+                        .then(
+                            async (res) => {
+                                
+                                fetch(res.data[0])
+                                    .then(
+                                        (response) => {
+                                            console.log(response)
+                                            let reader = response.body.getReader();
+                                            let decoder = new TextDecoder('utf-8');
+                            
+                                            reader.read()
+                                                .then(
+                                                    (result) => {
+                                                        //console.log(decoder.decode(result.value));
+                                                        const csv = decoder.decode(result.value)
+                                                        const parsedCsv = Papa.parse(csv, {header:true})
+                                                        this.setState({data:parsedCsv.data})
+                                                        //console.log(parsedCsv.meta)   ->表头
+
+                                                });
+                                    });
+
+                            }
+                        )
+                        .catch(
+                            (err) => {
+                                console.log(err)
+                            }
+                        )
                 }
             )
             .catch(
@@ -33,9 +72,14 @@ export default class C2INew extends Component {
                     console.log(err)
                 }
             )
+        
     }
     render() {
         const keyboard = this.state.keyboard
+        var columns = []
+        for (var j in this.state.data[0]) {
+            columns.push({ title: j, dataIndex: j, key: j })
+        }
         return (
             <div>
                 <Space>
@@ -62,6 +106,11 @@ export default class C2INew extends Component {
                 <button onClick={this.confirm}>
                     确认
                 </button>
+                <Table
+                    style={{display: this.state.data === [] ? 'none' : 'block' }}
+                    columns={columns}
+                    dataSource={this.state.data}/>
+
             </div>
 
         )
