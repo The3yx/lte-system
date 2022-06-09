4474f638fc9660f86d26fd973c86566664352590
The3yx <1340175409@qq.com>
2022-06-09 11:48:34 +0800

    nightly_update

--- src/pages/C2INew/index.jsx
+++ src/pages/C2INew/index.jsx
@@ -37,7 +37,7 @@ export default class C2INew extends Component {
                         }
                     })
                         .then(
-                            async (res) => {
+                            (res) => {
                                 
                                 fetch(res.data[0])
                                     .then(
